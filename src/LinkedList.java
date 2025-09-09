18fff609854564e925a95f1896193559e8d64462
buttivaishnavi <buttivaishnavi19@gmail.com>
2025-09-09 16:20:57 +0530

    Initial commit: Basic Linked List project

---
+++ src/LinkedList.java
@@ -0,0 +1,46 @@
+public class LinkedList {
+    Node head;
+
+    // Insert at end
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    // Delete a node
+    public void delete(int key) {
+        Node temp = head, prev = null;
+
+        if (temp != null && temp.data == key) {
+            head = temp.next;
+            return;
+        }
+
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if (temp == null) return;
+
+        prev.next = temp.next;
+    }
+
+    // Display list
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        // Insert elements
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+
+        System.out.println("Linked List after insertion:");
+        list.display();
+
+        // Delete element
+        list.delete(20);
+
+        System.out.println("Linked List after deleting 20:");
+        list.display();
+    }
+}
